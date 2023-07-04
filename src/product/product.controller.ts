865a03cc8df9c22ff2bb5831fc7116d49114012d
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2023-07-04 18:00:58 +0300

    feat(product): implement update product method

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('product')
 export class ProductController {
@@ -14,5 +15,10 @@ export class ProductController {
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productService.update(+id, updateProductDto);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
@@ -18,5 +19,9 @@ export class ProductService {
   create(createProductDto: CreateProductDto): Promise<Product> {
     const product = this.productRepository.create(createProductDto)
     return this.productRepository.save(product)
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto): Promise<Product> {
+    return this.productRepository.save({ id, ...updateProductDto })
   }
 }
